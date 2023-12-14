5790c0127ae2c12cc7b603d5bfcd580bcc4a96c6
markor2344 <153821077+markor2344@users.noreply.github.com>
2023-12-14 11:41:16 +0100

    Add files via upload

---
+++ aaaaaa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vezbao2
+{
+    class KlasaB
+    {
+        public string bezPrvogIZadnjeg(string tekst)
+        {
+            tekst = tekst.Substring(1, tekst.Length - 2);
+            return tekst;
+        }
+    }
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+            KlasaB objekt = new KlasaB();
+            Console.WriteLine("Upisi neki tekst duzi od dva znaka.");
+            string tekst = Console.ReadLine();
+
+            if(tekst.Length > 2)
+            {
+                Console.WriteLine(objekt.bezPrvogIZadnjeg(tekst));
+            }
+            else
+            {
+                Console.WriteLine("Tekst nije duzi od 2 znaka.");
+            }
+
+            Console.ReadKey();
+             
+            }
+    }
+    
+}
