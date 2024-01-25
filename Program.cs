e39774dd32c0996f084a7396f3c9c0fa74b28dec
markor2344 <153821077+markor2344@users.noreply.github.com>
2024-01-25 11:03:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljedivanje
+{
+    class Cat
+    {
+        bool opasna;
+
+        public bool Opasna { get => opasna; set => opasna = value; }
+
+       public virtual void Opasnost()
+        {
+            Opasna = false;
+        }
+    }
+
+    class Gepard : Cat 
+    {
+        public override void Opasnost()
+        {
+            Opasna = true;
+        }
+    }
+
+
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Gepard g = new Gepard();
+            Console.WriteLine(g.Opasna);
+            g.Opasnost();
+
+            Console.WriteLine(g.Opasna);
+
+            Console.ReadKey();
+         
+        }
+    }
+}
