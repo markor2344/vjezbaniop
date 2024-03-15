45912b4a1060d82e01324b9da1596791cb7695a2
markor2344 <153821077+markor2344@users.noreply.github.com>
2024-03-15 11:04:59 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Nasljedivanje
+class Student
 {
-    class Cat
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        bool opasna;
+        Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
 
-        public bool Opasna { get => opasna; set => opasna = value; }
+        Student[] students = new Student[10];
 
-       public virtual void Opasnost()
+        // int i = 0;
+
+        var linqSwanje = from s in studentArray
+                         where s.Age > 12 && s.Age < 20
+                         select s;
+
+        foreach (Student s in linqSwanje)
         {
-            Opasna = false;
+            /* if (std.Age > 12 && std.Age < 20)
+             {
+                 students[i] = std;
+                 i++;
+             }*/
+            Console.WriteLine(s.StudentName);
         }
-    }
+        Console.ReadKey();
 
-    class Gepard : Cat 
-    {
-        public override void Opasnost()
-        {
-            Opasna = true;
-        }
-    }
-
-
-    internal class Program
-    { 
-        static void Main(string[] args)
-        {
-            Gepard g = new Gepard();
-            Console.WriteLine(g.Opasna);
-            g.Opasnost();
-
-            Console.WriteLine(g.Opasna);
-
-            Console.ReadKey();
-         
-        }
     }
 }
