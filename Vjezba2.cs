24e80e303019beaaf00f83ee8595ae2d23eb596d
markor2344 <153821077+markor2344@users.noreply.github.com>
2024-03-15 10:55:13 +0100

    Add files via upload

---
+++ Vjezba2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // Student collection
+        List<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John" } ,
+                new Student() { StudentID = 2, StudentName = "Moin" } ,
+                new Student() { StudentID = 3, StudentName = "Bill" } ,
+                new Student() { StudentID = 4, StudentName = "Ram" } ,
+                new Student() { StudentID = 5, StudentName = "Ron"  }
+            };
+
+        var rezultat = from studenac in studentList
+                       where studenac.StudentName.Contains('o')
+                       select studenac.StudentName;
+        foreach (var rez in rezultat)
+        {
+            Console.WriteLine(rez.ToString());
+        }
+        Console.ReadKey();
+    }
+}
+
+
+public class Student
+{
+
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+}
