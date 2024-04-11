e69e4dc1fb9dbc0dee863b699a4c175188c69376
markor2344 <153821077+markor2344@users.noreply.github.com>
2024-04-11 11:00:54 +0200

    Add files via upload

---
+++ Ispitzadatak2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ispitzadatak2
+{
+    internal class Program
+    {
+        class Zrakoplov
+        {
+            private string Naziv;
+            private double SnagaMotora;
+            private int DosegLeta;
+
+            public Zrakoplov(string naziv, double snagaMotora, int dosegLeta)
+            {
+                Naziv = naziv;
+                SnagaMotora = snagaMotora;
+                DosegLeta = dosegLeta;
+            }
+
+            public void IspisiDetalje()
+            {
+                Console.WriteLine("Naziv: " + Naziv);
+                Console.WriteLine("Snaga motora: " + SnagaMotora + " kW");
+                Console.WriteLine("Doseg leta: " + DosegLeta + " km");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Zrakoplov x380 = new Zrakoplov("Airbus", 2000, 6000);
+            Zrakoplov C162 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+            Zrakoplov PC_21 = new Zrakoplov("Pilatus", 1200, 1333);
+
+            Console.WriteLine("Detalji o zrakoplovima:");
+            x380.IspisiDetalje();
+            Console.WriteLine();
+            C162.IspisiDetalje();
+            Console.WriteLine();
+            PC_21.IspisiDetalje();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
