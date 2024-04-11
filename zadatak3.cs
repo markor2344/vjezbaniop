a6a10e2e59fc5eac1d64a713b56e0bed8e3ec076
markor2344 <153821077+markor2344@users.noreply.github.com>
2024-04-11 11:13:20 +0200

    Add files via upload

---
+++ zadatak3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace zadatak3
+{
+    class Vozilo
+    {
+        public bool JeLiLeti { get; set; }
+        public bool JeLiPlovi { get; set; }
+
+        public void KudaVozi()
+        {
+            Console.WriteLine("Je li vozilo leti: " + JeLiLeti);
+            Console.WriteLine("Je li vozilo plovi: " + JeLiPlovi);
+        }
+    }
+
+    class Brod : Vozilo
+    {
+        public Brod(bool jeLiPlovi)
+        {
+            JeLiPlovi = jeLiPlovi;
+            JeLiLeti = false;
+        }
+    }
+
+    class Zrakoplov : Vozilo
+    {
+        public Zrakoplov(bool jeLiLeti)
+        {
+            JeLiLeti = jeLiLeti;
+            JeLiPlovi = false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Brod brod1 = new Brod(true);
+            Brod brod2 = new Brod(false);
+            Zrakoplov zrakoplov1 = new Zrakoplov(true);
+            Zrakoplov zrakoplov2 = new Zrakoplov(false);
+
+            Console.WriteLine("Vrijednosti varijabli za brod1:");
+            brod1.KudaVozi();
+            Console.WriteLine();
+
+            Console.WriteLine("Vrijednosti varijabli za brod2:");
+            brod2.KudaVozi();
+            Console.WriteLine();
+
+            Console.WriteLine("Vrijednosti varijabli za zrakoplov1:");
+            zrakoplov1.KudaVozi();
+            Console.WriteLine();
+
+            Console.WriteLine("Vrijednosti varijabli za zrakoplov2:");
+            zrakoplov2.KudaVozi();
+            Console.WriteLine();
+        }
+    }
+}
